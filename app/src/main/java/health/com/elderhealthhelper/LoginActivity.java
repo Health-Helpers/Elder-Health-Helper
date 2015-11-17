9e7097e027804bf3814779eee590a4891c151946
Marc Pifarré Montalà <pifarremarc@gmail.com>
2015-11-17 19:21:30 +0100

    1st

--- app/src/main/java/health/com/elderhealthhelper/LoginActivity.java
+++ app/src/main/java/health/com/elderhealthhelper/LoginActivity.java
@@ -2,13 +2,35 @@ package health.com.elderhealthhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView loginStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginStatus = (TextView) findViewById(R.id.login_status);
+        String phoneNumber = checkPhoneNumber();
+        String smsCode = checkSmsCode();
+    }
+
+    private String checkSmsCode() {
+        return null;
+    }
+
+    private String checkPhoneNumber() {
+
+        return null;
+    }
+
+    private interface processListenner{
+        public Object onSuccess (Object object);
+        public Object onFailure (Object object);
     }
 
 }
---
+++ app/src/main/java/health/com/elderhealthhelper/utils/ProcessListenner.java
@@ -0,0 +1,9 @@
+package health.com.elderhealthhelper.utils;
+
+/**
+ * Created by mpifa on 15/11/15.
+ */
+public interface ProcessListenner{
+    public Object onSuccess (Object object);
+    public Object onFailure (Object object);
+}
