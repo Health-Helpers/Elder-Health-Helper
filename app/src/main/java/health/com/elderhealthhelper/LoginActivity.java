d88fdb531ac522a7f201dfcfee933a9170193f4b
Marc Pifarré Montalà <pifarremarc@gmail.com>
2015-11-18 20:04:55 +0100

    Login almost finished + webservice defined + SMS receiver (not finished)

--- app/src/main/java/health/com/elderhealthhelper/LoginActivity.java
+++ app/src/main/java/health/com/elderhealthhelper/LoginActivity.java
@@ -1,10 +1,17 @@
 package health.com.elderhealthhelper;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
-import java.util.Objects;
+
+import com.android.volley.VolleyError;
+
+import health.com.elderhealthhelper.networking.WebServiceConnection;
+import health.com.elderhealthhelper.sms.SmSReceiver;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,21 +23,37 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         loginStatus = (TextView) findViewById(R.id.login_status);
         String phoneNumber = checkPhoneNumber();
-        String smsCode = checkSmsCode();
+        WebServiceConnection webServiceConnection = WebServiceConnection.getInstance(getApplicationContext());
+        webServiceConnection.phoneNumberValidation(phoneNumber, new WebServiceConnection.CustomListener<String>(){
+
+            @Override
+            public void onSucces(String response) {
+
+            }
+
+            @Override
+            public void onError(VolleyError error) {
+
+            }
+        });
     }
 
-    private String checkSmsCode() {
+
+
+    private String checkSmsCode(String sms) {
         return null;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        String sms = extras.getString(SmSReceiver.SMS);
+        checkSmsCode(sms);
     }
 
     private String checkPhoneNumber() {
-
+        TelephonyManager tMgr = (TelephonyManager)getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        String mPhoneNumber = tMgr.getLine1Number();
         return null;
     }
-
-    private interface processListenner{
-        public Object onSuccess (Object object);
-        public Object onFailure (Object object);
-    }
-
 }
