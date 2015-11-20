87baf661ae864ce9046e99d983252476a9cb60e8
Ivan Josa <navijo@gmail.com>
2015-11-20 11:03:26 +0100

    Responsible Layout with a list of patients in a side menu. When an item is clicked, it shows a fragment with 2 more buttons

--- app/src/main/java/health/com/elderhealthhelper/responsible/MainActivity.java
+++ app/src/main/java/health/com/elderhealthhelper/responsible/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -18,8 +19,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import health.com.elderhealthhelper.R;
 
@@ -31,7 +42,7 @@ public class MainActivity extends Activity {
 
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
-    private String[] mPlanetTitles;
+    private String[] mPatientTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +50,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.responsible_activity_main);
 
         mTitle = mDrawerTitle = getTitle();
-        mPlanetTitles = getResources().getStringArray(R.array.patients_array);
+        mPatientTitles = getResources().getStringArray(R.array.patients_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
@@ -47,7 +58,7 @@ public class MainActivity extends Activity {
         mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
         // set up the drawer's list view with items and click listener
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.responsible_patient_list_item, mPlanetTitles));
+                R.layout.responsible_patient_list_item, mPatientTitles));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
         // enable ActionBar app icon to behave as action to toggle nav drawer
@@ -133,7 +144,7 @@ public class MainActivity extends Activity {
         // update the main content by replacing fragments
         Fragment fragment = new PatientFragment();
         Bundle args = new Bundle();
-        args.putInt(PatientFragment.ARG_PLANET_NUMBER, position);
+        args.putInt(PatientFragment.ARG_PATIENT_NUMBER, position);
         fragment.setArguments(args);
 
         FragmentManager fragmentManager = getFragmentManager();
@@ -141,7 +152,7 @@ public class MainActivity extends Activity {
 
         // update selected item and title, then close the drawer
         mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
+        setTitle(mPatientTitles[position]);
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 
@@ -171,29 +182,132 @@ public class MainActivity extends Activity {
     }
 
     /**
-     * Fragment that appears in the "content_frame", shows a planet
+     * Fragment that appears in the "content_frame", shows a patient
      */
     public static class PatientFragment extends Fragment {
-        public static final String ARG_PLANET_NUMBER = "planet_number";
+        public static final String ARG_PATIENT_NUMBER = "patient_number";
+        public static FragmentManager fragmentManager;
 
         public PatientFragment() {
             // Empty constructor required for fragment subclasses
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.responsible_patient_fragment, container, false);
-            int i = getArguments().getInt(ARG_PLANET_NUMBER);
+            int i = getArguments().getInt(ARG_PATIENT_NUMBER);
            String patient = getResources().getStringArray(R.array.patients_array)[i];
 
 
+            // initialising the object of the FragmentManager. Here I'm passing getSupportFragmentManager(). You can pass getFragmentManager() if you are coding for Android 3.0 or above.
+            fragmentManager = getFragmentManager();
 
-            /*int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
-                    "drawable", getActivity().getPackageName());
-            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);*/
+            Button locatePatientBtn = (Button) rootView.findViewById(R.id.localizePatientBtn);
+            locatePatientBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Context context = v.getContext();
+
+                    Fragment fragment = new PatientMapFragment();
+
+                    FragmentManager fragmentManager = getFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+                    Toast.makeText(context, "Locate Pressed", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+
             getActivity().setTitle(patient);
             return rootView;
+        }
+    }
+
+    public static class PatientMapFragment extends Fragment {
+
+        private static View view;
+        public static FragmentManager fragmentManager;
+        /**
+         * Note that this may be null if the Google Play services APK is not
+         * available.
+         */
+
+        MapView mMapView;
+        private GoogleMap googleMap;
+        private static GoogleMap mMap;
+        private static Double latitude, longitude;
+
+        public static final String ARG_PATIENT_NUMBER = "patient_number";
+
+        public PatientMapFragment() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // inflat and return the layout
+            View v = inflater.inflate(R.layout.responsible_patient_map, container,
+                    false);
+            mMapView = (MapView) v.findViewById(R.id.location_map);
+            mMapView.onCreate(savedInstanceState);
+
+            mMapView.onResume();// needed to get the map to display immediately
+
+            try {
+                MapsInitializer.initialize(getActivity().getApplicationContext());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            googleMap = mMapView.getMap();
+            // latitude and longitude
+            double latitude = 41.618341;
+            double longitude = 0.6024253;
+
+            // create marker
+            MarkerOptions marker = new MarkerOptions().position(
+                    new LatLng(latitude, longitude)).title("Hello Maps");
+
+            // Changing marker icon
+            marker.icon(BitmapDescriptorFactory
+                    .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+            // adding marker
+            googleMap.addMarker(marker);
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(latitude, longitude)).zoom(12).build();
+            googleMap.animateCamera(CameraUpdateFactory
+                    .newCameraPosition(cameraPosition));
+
+            // Perform any camera updates here
+            return v;
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            mMapView.onResume();
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            mMapView.onPause();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            mMapView.onDestroy();
+        }
+
+        @Override
+        public void onLowMemory() {
+            super.onLowMemory();
+            mMapView.onLowMemory();
         }
     }
 }
