eb439f6b481d6c67cfeabb02c736bd8e0bdfd128
solacarol <sola.carol@gmail.com>
2015-11-23 20:46:35 +0100

    Criado clase Bletooth y Método de búsqueda para el dispositivo

--- app/src/main/java/com/hh/ehh/patient/PatientHomeFragment.java
+++ app/src/main/java/com/hh/ehh/patient/PatientHomeFragment.java
@@ -1,8 +1,10 @@
 package com.hh.ehh.patient;
 
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,32 +12,67 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.hh.ehh.R;
+import com.hh.ehh.bluetooth.Bluetooth;
+
+import java.util.ArrayList;
 
 /**
  * Created by mpifa on 23/11/15.
  */
-public class PatientHomeFragment extends Fragment {
+public class PatientHomeFragment extends Fragment implements Bluetooth.BluetoothListener{
 
-    private Button pairButton;
-    private Button callButton;
-    private Button settingsButton;
+    private Button btnBluetooth;
+
+    private static final String TAG = "FindBluetooth";
+    private Bluetooth bluetooth;
+    private ArrayList<String> arrayAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.patient_activity_main, container, false);
-        pairButton = (Button) view.findViewById(R.id.btnNuevoResponsable);
-        callButton = (Button) view.findViewById(R.id.callBtn);
-        settingsButton = (Button) view.findViewById(R.id.settingsBtn);
+        btnBluetooth = (Button) view.findViewById(R.id.btnBluetooth);
 
-        pairButton.setOnClickListener(new View.OnClickListener() {
+        try {
+            bluetooth = Bluetooth.startFindDevices(getActivity(), this);
+        } catch (Exception e) {
+            Log.e(TAG, "Error: ", e);
+        }
+
+        arrayAdapter = new ArrayList<String>();
+
+
+
+        btnBluetooth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Context context = v.getContext();
-                Toast.makeText(context, "Pair Pressed", Toast.LENGTH_SHORT).show();
+                for (String name : arrayAdapter) {
+                    Toast.makeText(context, name, Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
         return view;
+    }
+
+    @Override
+    public void action(String action) {
+
+        if (action.compareTo(ACTION_DISCOVERY_STARTED) == 0) {
+            arrayAdapter.add("-Started Find");
+        } else if (action.compareTo(ACTION_DISCOVERY_FINISHED) == 0) {
+            preencherLista();
+            arrayAdapter.add("-Finished Find");
+        }
+    }
+
+    private void preencherLista() {
+        for (BluetoothDevice device : bluetooth.getDispositivos()) {
+            arrayAdapter.add(device.getName() + "\n" + device.getAddress());
+        }
     }
 
     @Override
